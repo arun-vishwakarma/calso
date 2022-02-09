bb4da814f455e6dc428d8e9e5be4c4d6c53a553f
Arun Kr Vshwakarma <mca.arun9@gmail.com>
2022-02-09 14:27:22 +0530

    remove zoom video done

--- pages/api/integrations.ts
+++ pages/api/integrations.ts
@@ -3,6 +3,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "@lib/auth";
 import prisma from "@lib/prisma";
 
+const client_id = process.env.ZOOM_CLIENT_ID;
+const client_secret = process.env.ZOOM_CLIENT_SECRET;
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!["GET", "DELETE"].includes(req.method!)) {
     return res.status(405).end();
@@ -30,6 +33,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   if (req.method == "DELETE") {
+    //remove zoom video from zoom account
+    const credentials = await prisma.credential.findFirst({
+      where: {
+        userId: session.user?.id,
+        type: "zoom_video",
+      },
+      select: {
+        type: true,
+        key: true,
+      },
+    });
+    const authHeader = "Basic " + Buffer.from(client_id + ":" + client_secret).toString("base64");
+    //console.log('authHeader',authHeader);
+    const result = await fetch("https://zoom.us/oauth/revoke", {
+      method: "POST",
+      headers: {
+        Authorization: authHeader,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "token=" + credentials.key.access_token,
+    });
+    const responseBody = await result.json();
+    console.log("revoke zoom_video", responseBody);
+
+    //remove zoom video from db
     const id = req.body.id;
 
     await prisma.user.update({
